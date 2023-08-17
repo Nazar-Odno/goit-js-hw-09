6da107436ef8f49f56d218a8a3139f36e1d5d302
Nazar-Odno <114081040+Nazar-Odno@users.noreply.github.com>
2023-08-17 18:59:28 +0300

    homework

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,47 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+	form: document.querySelector('.form'),
+	createPromisesBtn: document.querySelector('.form > button'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(evt) {
+	evt.preventDefault();
+	
+	let delay = refs.form.elements.delay.valueAsNumber;
+	const delayStep = refs.form.elements.step.valueAsNumber;
+	const amount = refs.form.elements.amount.valueAsNumber;
+	refs.createPromisesBtn.disabled = true;
+	setTimeout(() => {
+		refs.createPromisesBtn.disabled = false;
+	}, delay + delayStep*amount)
+	
+	for (let position = 1; position <= amount; position += 1) {
+		createPromise(position, delay)
+			.then(({ position, delay }) => {
+				Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+			})
+			.catch(({ position, delay }) => {
+				Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+			});
+		delay += delayStep;
+	}
+};
+
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+	const shouldResolve = Math.random() > 0.3;
+	return new Promise((resolve, reject) => {
+		if (shouldResolve) {
+			setTimeout(() => {
+				resolve({ position, delay });
+				}, delay);
+		} else {
+			setTimeout(() => {
+				reject({ position, delay });
+			}, delay)
+		};
+	});
+};
